ec797a1ea065849c05eb14764bbe7194f421a61f
Cmoonie <ceciliaanim9@gmail.com>
2025-01-06 16:26:09 +0100

    uitloggen knop verwijdert

--- profile.php
+++ profile.php
@@ -13,7 +13,7 @@ $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Cecilia's Portfolio</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="/public/css/stylesheet.css">
 
@@ -21,7 +21,7 @@ $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <body>
 <header class="nav">
     <a class="active" href="/">Home</a>
-    <a href="/uitloggen">Uitloggen</a>
+
 <!--    --><?php
 //    if (!empty($error)) { echo $error; }
 //    ?>
